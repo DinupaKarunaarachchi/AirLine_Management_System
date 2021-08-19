2665e1c811e3db5406473117dc028de2848b8191
Dinupa Karunaarachchi <86536976+DinupaKarunaarachchi@users.noreply.github.com>
2021-08-19 22:02:22 +0530

    form login

---
+++ Source Code/Airline_M_S/Formlogin.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Airline_M_S
+{
+    partial class Formlogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formlogin));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbllogin = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblusrnamee = new System.Windows.Forms.Label();
+            this.lblpsswrd = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonlogin = new System.Windows.Forms.Button();
+            this.buttonexit = new System.Windows.Forms.Button();
+            this.btnadmin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(423, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 497);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbllogin
+            // 
+            this.lbllogin.AutoSize = true;
+            this.lbllogin.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllogin.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbllogin.Location = new System.Drawing.Point(112, 53);
+            this.lbllogin.Name = "lbllogin";
+            this.lbllogin.Size = new System.Drawing.Size(195, 81);
+            this.lbllogin.TabIndex = 1;
+            this.lbllogin.Text = "LogIn";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(76, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblusrnamee
+            // 
+            this.lblusrnamee.AutoSize = true;
+            this.lblusrnamee.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblusrnamee.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblusrnamee.Location = new System.Drawing.Point(12, 188);
+            this.lblusrnamee.Name = "lblusrnamee";
+            this.lblusrnamee.Size = new System.Drawing.Size(143, 32);
+            this.lblusrnamee.TabIndex = 3;
+            this.lblusrnamee.Text = "Username :";
+            // 
+            // lblpsswrd
+            // 
+            this.lblpsswrd.AutoSize = true;
+            this.lblpsswrd.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpsswrd.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblpsswrd.Location = new System.Drawing.Point(12, 253);
+            this.lblpsswrd.Name = "lblpsswrd";
+            this.lblpsswrd.Size = new System.Drawing.Size(137, 32);
+            this.lblpsswrd.TabIndex = 4;
+            this.lblpsswrd.Text = "Password :";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(124)))));
+            this.panel1.Location = new System.Drawing.Point(18, 218);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 2);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(117)))), ((int)(((byte)(124)))));
+            this.panel2.Location = new System.Drawing.Point(18, 283);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(368, 2);
+            this.panel2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox1.Location = new System.Drawing.Point(161, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 23);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textBox2.Location = new System.Drawing.Point(161, 259);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 23);
+            this.textBox2.TabIndex = 13;
+            // 
+            // buttonlogin
+            // 
+            this.buttonlogin.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonlogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonlogin.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonlogin.FlatAppearance.BorderSize = 0;
+            this.buttonlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonlogin.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonlogin.ForeColor = System.Drawing.Color.Black;
+            this.buttonlogin.Location = new System.Drawing.Point(58, 331);
+            this.buttonlogin.Name = "buttonlogin";
+            this.buttonlogin.Size = new System.Drawing.Size(306, 40);
+            this.buttonlogin.TabIndex = 14;
+            this.buttonlogin.Text = "LOGIN";
+            this.buttonlogin.UseVisualStyleBackColor = false;
+            this.buttonlogin.Click += new System.EventHandler(this.buttonlogin_Click);
+            // 
+            // buttonexit
+            // 
+            this.buttonexit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.buttonexit.FlatAppearance.BorderSize = 0;
+            this.buttonexit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonexit.ForeColor = System.Drawing.Color.Red;
+            this.buttonexit.Location = new System.Drawing.Point(149, 455);
+            this.buttonexit.Name = "buttonexit";
+            this.buttonexit.Size = new System.Drawing.Size(113, 42);
+            this.buttonexit.TabIndex = 15;
+            this.buttonexit.Text = "EXIT";
+            this.buttonexit.UseVisualStyleBackColor = false;
+            this.buttonexit.Click += new System.EventHandler(this.buttonexit_Click);
+            // 
+            // btnadmin
+            // 
+            this.btnadmin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.btnadmin.FlatAppearance.BorderSize = 0;
+            this.btnadmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnadmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(148)))), ((int)(((byte)(208)))));
+            this.btnadmin.Location = new System.Drawing.Point(126, 377);
+            this.btnadmin.Name = "btnadmin";
+            this.btnadmin.Size = new System.Drawing.Size(167, 42);
+            this.btnadmin.TabIndex = 16;
+            this.btnadmin.Text = "ADMIN";
+            this.btnadmin.UseVisualStyleBackColor = false;
+            // 
+            // Formlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.ClientSize = new System.Drawing.Size(1023, 497);
+            this.Controls.Add(this.btnadmin);
+            this.Controls.Add(this.buttonexit);
+            this.Controls.Add(this.buttonlogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblpsswrd);
+            this.Controls.Add(this.lblusrnamee);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbllogin);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Formlogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formlogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbllogin;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblusrnamee;
+        private System.Windows.Forms.Label lblpsswrd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonlogin;
+        private System.Windows.Forms.Button buttonexit;
+        private System.Windows.Forms.Button btnadmin;
+    }
+}
---
+++ Source Code/Airline_M_S/Formlogin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airline_M_S
+{
+    public partial class Formlogin : Form
+    {
+        public Formlogin()
+        {
+            Thread th = new Thread(new ThreadStart(Loading));
+            th.Start();
+            InitializeComponent();
+
+            for(int i=0;i<=1000;i++)
+            {
+                Thread.Sleep(10);
+            }
+            th.Abort();
+        }
+
+        void Loading()
+        {
+            Formloading sp = new Formloading();
+            Application.Run(sp);
+        }
+
+        private void buttonlogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Formhome Y = new Formhome();
+            Y.Show();
+            
+        }
+
+        private void buttonexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
