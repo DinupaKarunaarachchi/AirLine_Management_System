785930988b9ec48b830bed73937bd0086cd01fdc
Dinupa Karunaarachchi <86536976+DinupaKarunaarachchi@users.noreply.github.com>
2021-08-19 22:01:13 +0530

    form home

---
+++ Source Code/Airline_M_S/Formhome.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Airline_M_S
+{
+    partial class Formhome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formhome));
+            this.pnlhome = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblflight = new System.Windows.Forms.Label();
+            this.lblpssnger = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbltickts = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblcancl = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.lbldashbrd = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.lblhome = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pnlhome.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlhome
+            // 
+            this.pnlhome.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlhome.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlhome.Controls.Add(this.pictureBox7);
+            this.pnlhome.Controls.Add(this.lbldashbrd);
+            this.pnlhome.Controls.Add(this.pictureBox5);
+            this.pnlhome.Controls.Add(this.lblcancl);
+            this.pnlhome.Controls.Add(this.pictureBox4);
+            this.pnlhome.Controls.Add(this.lbltickts);
+            this.pnlhome.Controls.Add(this.pictureBox3);
+            this.pnlhome.Controls.Add(this.lblpssnger);
+            this.pnlhome.Controls.Add(this.pictureBox2);
+            this.pnlhome.Controls.Add(this.lblflight);
+            this.pnlhome.Controls.Add(this.pictureBox1);
+            this.pnlhome.Location = new System.Drawing.Point(57, 166);
+            this.pnlhome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlhome.Name = "pnlhome";
+            this.pnlhome.Size = new System.Drawing.Size(940, 492);
+            this.pnlhome.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblflight
+            // 
+            this.lblflight.AutoSize = true;
+            this.lblflight.BackColor = System.Drawing.Color.Transparent;
+            this.lblflight.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblflight.ForeColor = System.Drawing.Color.White;
+            this.lblflight.Location = new System.Drawing.Point(43, 155);
+            this.lblflight.Name = "lblflight";
+            this.lblflight.Size = new System.Drawing.Size(107, 32);
+            this.lblflight.TabIndex = 1;
+            this.lblflight.Text = "Flights";
+            // 
+            // lblpssnger
+            // 
+            this.lblpssnger.AutoSize = true;
+            this.lblpssnger.BackColor = System.Drawing.Color.Transparent;
+            this.lblpssnger.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpssnger.ForeColor = System.Drawing.Color.White;
+            this.lblpssnger.Location = new System.Drawing.Point(381, 155);
+            this.lblpssnger.Name = "lblpssnger";
+            this.lblpssnger.Size = new System.Drawing.Size(175, 32);
+            this.lblpssnger.TabIndex = 3;
+            this.lblpssnger.Text = "Passengers";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(409, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbltickts
+            // 
+            this.lbltickts.AutoSize = true;
+            this.lbltickts.BackColor = System.Drawing.Color.Transparent;
+            this.lbltickts.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltickts.ForeColor = System.Drawing.Color.White;
+            this.lbltickts.Location = new System.Drawing.Point(789, 155);
+            this.lbltickts.Name = "lbltickts";
+            this.lbltickts.Size = new System.Drawing.Size(112, 32);
+            this.lbltickts.TabIndex = 5;
+            this.lbltickts.Text = "Tickets";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(778, 29);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lblcancl
+            // 
+            this.lblcancl.AutoSize = true;
+            this.lblcancl.BackColor = System.Drawing.Color.Transparent;
+            this.lblcancl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcancl.ForeColor = System.Drawing.Color.White;
+            this.lblcancl.Location = new System.Drawing.Point(189, 362);
+            this.lblcancl.Name = "lblcancl";
+            this.lblcancl.Size = new System.Drawing.Size(186, 32);
+            this.lblcancl.TabIndex = 7;
+            this.lblcancl.Text = "Cancellation";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(220, 234);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // lbldashbrd
+            // 
+            this.lbldashbrd.AutoSize = true;
+            this.lbldashbrd.BackColor = System.Drawing.Color.Transparent;
+            this.lbldashbrd.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldashbrd.ForeColor = System.Drawing.Color.White;
+            this.lbldashbrd.Location = new System.Drawing.Point(582, 362);
+            this.lbldashbrd.Name = "lbldashbrd";
+            this.lbldashbrd.Size = new System.Drawing.Size(166, 32);
+            this.lbldashbrd.TabIndex = 9;
+            this.lbldashbrd.Text = "DashBoard";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(603, 234);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(123, 123);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // lblhome
+            // 
+            this.lblhome.AutoSize = true;
+            this.lblhome.BackColor = System.Drawing.Color.Transparent;
+            this.lblhome.Font = new System.Drawing.Font("Adobe Caslon Pro Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(50)))), ((int)(((byte)(95)))));
+            this.lblhome.Location = new System.Drawing.Point(330, 62);
+            this.lblhome.Name = "lblhome";
+            this.lblhome.Size = new System.Drawing.Size(370, 102);
+            this.lblhome.TabIndex = 2;
+            this.lblhome.Text = "SL_AirLines";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(285, 23);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(70, 60);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(452, 445);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(46, 44);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // Formhome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1056, 707);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.lblhome);
+            this.Controls.Add(this.pnlhome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Formhome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Formhome";
+            this.pnlhome.ResumeLayout(false);
+            this.pnlhome.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlhome;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblflight;
+        private System.Windows.Forms.Label lblcancl;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lbltickts;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblpssnger;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbldashbrd;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label lblhome;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
